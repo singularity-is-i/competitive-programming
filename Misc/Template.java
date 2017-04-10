4b59f75973637e9faa15ed3c39dab805a3f99092
Wilson Ding <shodai100@gmail.com>
2017-04-10 09:12:22 -0500

    Updated Template to add code for cases

--- Misc/Template.java
+++ Misc/Template.java
@@ -5,6 +5,10 @@ class Template {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
-		int value = scan.nextInt();
+		int totalCases = scan.nextInt();
+
+		for(int currentCase = 0; currentCase < totalCases; currentCase++) {
+			// Do work for each test case here...
+		}
 	}
 }
