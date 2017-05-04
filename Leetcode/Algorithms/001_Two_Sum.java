2c8cfc3fe02f5964a16ea79618e3db20b458ecab
Wilson Ding <shodai100@gmail.com>
2017-05-04 14:56:15 -0500

    Added solution 001_Two_Sum for LeetCode

---
+++ Leetcode/Algorithms/001_Two_Sum.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Two_Sum {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> compliments = new HashMap<>();
+
+		for(int index = 0; index < nums.length; index++) {
+			if(compliments.containsKey(nums[index])) {
+				return new int[]{(int)compliments.get(nums[index]), index};
+			} else {
+				compliments.put(target - nums[index], index);
+			}
+		}
+
+		throw new IllegalArgumentException("No two sum solution");
+    }
+}
