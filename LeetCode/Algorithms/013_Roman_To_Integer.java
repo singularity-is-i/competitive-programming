6f3d449c0163aa0ab5614d3ba4c11f38403511cf
Wilson Ding <hello@wilsonding.com>
2017-05-10 18:02:03 -0500

    Added solution 013 for Leetcode

---
+++ LeetCode/Algorithms/013_Roman_To_Integer.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class Roman_To_Integer {
+	public static int getValue(char letter) {
+		switch (letter) {
+			case 'I':	return 1;
+			case 'V':	return 5;
+			case 'X':	return 10;					
+			case 'L':	return 50;
+			case 'C':	return 100;
+			case 'D':	return 500;
+			case 'M':	return 1000;
+			default:	return 0;
+		}
+	}
+
+	public static int romanToInt(String s) {
+		int number = 0;
+		int lastValue = 0;
+
+		for(int i = 0; i < s.length(); i++) {
+			int value = getValue(s.charAt(i));
+
+			if(i != 0 && value > lastValue) {
+				number -= (2 * lastValue);
+			}
+
+			number += value;
+			lastValue = value;
+		}
+
+		return number;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(romanToInt("MMMXLVIII"));
+	}
+}
