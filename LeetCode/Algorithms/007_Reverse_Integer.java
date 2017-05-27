3c7b530b9a38bca033f34a603dc42961e9c8ebdc
Wilson Ding <hello@wilsonding.com>
2017-05-27 17:48:04 -0500

    Added solution 007 for LeetCode

---
+++ LeetCode/Algorithms/007_Reverse_Integer.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int reverse(int x) {
+        long answer = 0;
+        
+        while(x != 0) {
+            answer = answer * 10 + x % 10;
+            x /= 10;
+            
+            if(answer > Integer.MAX_VALUE || answer < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        
+        return (int) answer;
+    }
+}
